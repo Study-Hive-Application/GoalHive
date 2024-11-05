1db581bae909ef5d5ca4530e6841defe3daa0faa
CodeAnup <165381842+CodeAnup@users.noreply.github.com>
2024-11-05 21:53:27 +0530

    Add files via upload

---
+++ features.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-btn').onclick = function () {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+  };
+  function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+  }
