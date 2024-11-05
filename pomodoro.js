d876cfa999a423d1719f31dae7b00b4c5f38af85
CodeAnup <165381842+CodeAnup@users.noreply.github.com>
2024-11-05 21:46:25 +0530

    Update pomodoro.js

--- pomodoro.js
+++ pomodoro.js
@@ -125,3 +125,11 @@ resetButton.addEventListener("click", openResetPopup);
 // Initial Setup
 updateTimerDisplay();
 updateProgressCircle();
+
+document.getElementById('menu-btn').onclick = function () {
+  document.getElementById('mobile-menu').classList.toggle('hidden');
+};
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+}
