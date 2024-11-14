1f633d5ac4163a3415be1212a93ce710c6a51799
Tanvipai <tanviipaii14@gmail.com>
2024-11-14 11:54:54 +0530

    Updated Db Connnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
+const asyncHandler = require("express-async-handler");
 
-const connectDB = async () => {
-  try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(
-      "Connected to MongoDB host : ",
-      connect.connection.host,
-      "\tname : ",
-      connect.connection.name
-    );
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
+const connectDB = asyncHandler(async () => {
+  const connect = await mongoose.connect(process.env.MONGO_URI);
+  console.log(
+    "Connected to MongoDB host : ",
+    connect.connection.host,
+    "\tname : ",
+    connect.connection.name
+  );
+});
 
 module.exports = connectDB;
